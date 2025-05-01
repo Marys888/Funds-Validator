3f75a90bde448e8f733aed015c7fda606144a461
Marys888 <severovamaha1999@gmail.com>
2025-05-01 17:42:48 +0200

    Add balance handler implementation

---
+++ src/app/BalanceHandler.java
@@ -0,0 +1,18 @@
+package app;
+
+public class BalanceHandler {
+    private double initialBalance;
+    public Balance(double initialBalance){
+        this.initialBalance = initialBalance;
+    }
+
+    public double getBalance(){
+        return  initialBalance;
+    }
+    public void purchase(double amount){
+        if(amount > initialBalance) {
+            throw new FundsException("Insufficient funds!");
+        }
+        initialBalance-= amount;
+    }
+}
